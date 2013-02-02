4246b67109fe23a8bd208db8963bf6e6db006165
David Mills <dave@1morepx.com>
2013-02-02 04:56:37 -0500

    refactored map logic into it's own namespace

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,3 +10,45 @@
 //= require leaflet
 //= require L.Control.Locate
 //= require_tree .
+
+var DTB = DTB || {}
+
+DTB.maps = {
+  
+  map: {},
+  init: function(latitude, longitude) {
+     this.map = L.map('map').setView([latitude, longitude], 17);
+
+    // add an OpenStreetMap tile layer
+    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+      maxZoom: 18,
+      attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(this.map);
+
+    // add Leaflet.Locate: A useful control to geolocate the user with many options.
+    L.control.locate({
+      position: 'topleft',  // set the location of the control
+      drawCircle: true,  // controls whether a circle is drawn that shows the uncertainty about the location
+      follow: false,  // follow the location if `watch` and `setView` are set to true in locateOptions
+      circleStyle: {},  // change the style of the circle around the user's location
+      markerStyle: {},
+      metric: true  // use metric or imperial units
+    }).addTo(this.map);
+
+    var bikeIcon = L.icon({
+      iconUrl: '/assets/marker-bike.png',
+      shadowUrl: '/assets/marker-shadow.png',
+
+      iconSize:     [32, 37], // size of the icon
+      shadowSize:   [41, 41], // size of the shadow
+      iconAnchor:   [20, 36], // point of the icon which will correspond to marker's location
+      shadowAnchor: [17, 38],  // the same for the shadow
+      popupAnchor:  [0, -26] // point from which the popup should open relative to the iconAnchor
+    });
+
+    var marker = L.marker([latitude, longitude], {icon: bikeIcon}).addTo(this.map);
+    marker.bindPopup('<strong>5/17</strong> bycicles,<br/> available <em>5 mins ago</em>.');
+    // marker.openPopup();
+  }
+
+}
--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -14,6 +14,8 @@ DTB.home = {
   loadLocation: function (location) {
     var latitude = location.coords.latitude;
     var longitude = location.coords.longitude;
+    
+    DTB.maps.init(latitude, longitude);
 
     $('#location').text('latitude: ' + latitude + ' longitude: ' + longitude);
     console.log(location.coords);
@@ -24,42 +26,13 @@ DTB.home = {
       success: DTB.home.getStations
     });
 
-    var map = L.map('map').setView([latitude, longitude], 17);
-
-    // add an OpenStreetMap tile layer
-    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-      maxZoom: 18,
-      attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
-
-    // add Leaflet.Locate: A useful control to geolocate the user with many options.
-    L.control.locate({
-      position: 'topleft',  // set the location of the control
-      drawCircle: true,  // controls whether a circle is drawn that shows the uncertainty about the location
-      follow: false,  // follow the location if `watch` and `setView` are set to true in locateOptions
-      circleStyle: {},  // change the style of the circle around the user's location
-      markerStyle: {},
-      metric: true  // use metric or imperial units
-    }).addTo(map);
-
-    var bikeIcon = L.icon({
-      iconUrl: '/assets/marker-bike.png',
-      shadowUrl: '/assets/marker-shadow.png',
-
-      iconSize:     [32, 37], // size of the icon
-      shadowSize:   [41, 41], // size of the shadow
-      iconAnchor:   [20, 36], // point of the icon which will correspond to marker's location
-      shadowAnchor: [17, 38],  // the same for the shadow
-      popupAnchor:  [0, -26] // point from which the popup should open relative to the iconAnchor
-    });
-
-    var marker = L.marker([latitude, longitude], {icon: bikeIcon}).addTo(map);
-    marker.bindPopup('<strong>5/17</strong> bycicles,<br/> available <em>5 mins ago</em>.');
-    // marker.openPopup();
+    
   },
 
   getStations: function (data, status, jqXHR) {
     var $stations = $('#stations');
+    
+    var map = DTB.maps.map;
     
     $stations.text('');
     for (var a in data) {
